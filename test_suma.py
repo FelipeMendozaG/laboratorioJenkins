d960fca0cd7b12d24c98543b1151c484783ca2ec
Felipe Mendoza <felipe188.mendoza@gmail.com>
2026-06-24 23:39:53 -0500

    correccion

--- test_suma.py
+++ test_suma.py
@@ -10,9 +10,7 @@ def test_all():
     assert suma(5,5) == 10
     assert multiplicar(5,5) == 25
     assert dividir(10,2)
-    assert dividir(10,0)
     assert es_par(2)
-    assert es_par(3)
     
 test_all()
 print("Prueba Exitosa")
