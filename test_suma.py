f9bff5960e8a4300f7c6c414d1edc85a0fd18fbf
Felipe Mendoza <felipe188.mendoza@gmail.com>
2026-06-24 23:35:17 -0500

    test

--- test_suma.py
+++ test_suma.py
@@ -1,6 +1,18 @@
 def suma(a,b):
     return a+b
-def test_suma():
+def multiplicar(a,b)
+    return a*b
+def dividir(a,b)
+    return a/b
+def es_par(numero):
+    return numero % 2 == 0
+def test_all():
     assert suma(5,5) == 10
-test_suma()
+    assert multiplicar(5,5) == 25
+    assert dividir(10,2)
+    assert dividir(10,0)
+    assert es_par(2)
+    assert es_par(3)
+    
+test_all()
 print("Prueba Exitosa")
